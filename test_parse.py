af049965c83ffffe041ab4ed8ea322f6138602f4
vm <carlguo866@gmail.com>
2021-06-26 14:28:48 -0400

    more graph

--- test_parse.py
+++
@@ -1,6 +0,0 @@
-import pyllvm
-
-if __name__ == "__main__": 
-    s =  "(i8* getelementptr inbounds ([15 x i8], [15 x i8]* @.str.898, i64 0, i64 0), i32 %5)" 
-    t = pyllvm.parseArgList(s)
-    print(t)
